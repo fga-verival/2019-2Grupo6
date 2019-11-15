996c1196fd087de2ff585bb84e8d6b51ff4b41da
Icaro Oliveira <icarooliv2@gmail.com>
2019-11-15 18:04:39 -0300

    second commit with falisfication

--- tbl2_f3/src/main/java/com/mycompany/aplicacao/Aplicacao.java
+++ tbl2_f3/src/main/java/com/mycompany/aplicacao/Aplicacao.java
@@ -3,5 +3,26 @@ package com.mycompany.aplicacao;
 
 public class Aplicacao 
 {
+    private int dias;
+
+    private float aplicacaoInicial;
+
+    private float juros;
+
+	public Aplicacao(int dias, float aplicacaoInicial, float juros) {
+
+	}
+
+	public float getRendimentoBruto() {
+		return 13.97F;
+    }
+    
+    public float getImpostoRenda() {
+		return 3.14F;
+	}
+
+	public float getRendimentoLiquido() {
+		return 1.0829F;
+	}
 
 }
